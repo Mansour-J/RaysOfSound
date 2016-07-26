7daf15dc7d3487cc4bb8707b5e7167b8e47d98b6
Josh Gomez <josh.gomez72@gmail.com>
2016-07-26 16:15:43 +1200

    Changed databasae to heroku and site can connect to it

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host : 'khmer.ecs.vuw.ac.nz',
-    user : 'root',
-    password : 'secret',
-    database : 'ros_test'
-});
+var pg = require('pg').native;
 
-console.log("connecting");
-connection.connect(function(err, results){
-   if (err) {
-        console.log("ERROR: " + err.message);
-        throw err;
-    }
-    console.log("connected.");
-})
+var database = "postgres://vxrebqhuwdodxj:4GH-oj44aX1A6W5i2OXIIfhViN@ec2-184-73-196-82.compute-1.amazonaws.com:5432/dddpqqg2ntj8g8?ssl=true";
+var client = new pg.Client(database);
+client.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+pg.connect(database, function(err, client, done){
+ 	if(err){
+ 		console.error('Could not connect to the database');
+ 		console.error(err);
+ 		return;
+ 	}
+ 	else {
+ 		console.log('connected to database')
+ 	}
+ });
 
 
 module.exports = router;
