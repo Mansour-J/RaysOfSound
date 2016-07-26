3e4b33eeb24edaadf48cfcdc85e7076fc4f339d7
Josh Gomez <josh.gomez72@gmail.com>
2016-07-26 17:53:32 +1200

    Using sequelize to manage databse, Only added two tables so far

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('pg').native;
-
-var database = "postgres://vxrebqhuwdodxj:4GH-oj44aX1A6W5i2OXIIfhViN@ec2-184-73-196-82.compute-1.amazonaws.com:5432/dddpqqg2ntj8g8?ssl=true";
-var client = new pg.Client(database);
-client.connect();
+var db = require('../lib/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-pg.connect(database, function(err, client, done){
- 	if(err){
- 		console.error('Could not connect to the database');
- 		console.error(err);
- 		return;
- 	}
- 	else {
- 		console.log('connected to database')
- 	}
- });
 
 
 module.exports = router;
