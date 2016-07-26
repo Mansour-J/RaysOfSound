fd204e483ff4ba368a4601e47423e73d45daed7b
Josh Gomez <josh.gomez72@gmail.com>
2016-07-26 15:35:27 +1200

    Fixed database connection for mysql

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host : 'khmer.ecs.vuw.ac.nz',
+    user : 'root',
+    password : 'secret',
+    database : 'ros_test'
+});
+
+console.log("connecting");
+connection.connect(function(err, results){
+   if (err) {
+        console.log("ERROR: " + err.message);
+        throw err;
+    }
+    console.log("connected.");
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
