3a67952b4dd892c008303958bf764dbc6f0e8bd0
Mansour Javaher <javaher.mansour@gmail.com>
2016-09-06 14:28:23 +1200

    Added the ecnryption for authentication -- Note::Package.JSON updated

---
+++ routes/category.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+//var db = require('../lib/db');
+
+router.get('/', function(req, res, next) {
+
+  res.render('category.ejs'/*TODO replace with categoy page*/, { title: 'Express' });
+
+
+});
+
+router.get('/:id/view', function(req, res, next) {
+
+  res.render('category.ejs'/*TODO replace with categoy page*/, { title: 'Express' });
+
+
+});
+
+router.get('/:id/edit', function(req, res, next) {
+
+  res.render('category.ejs'/*TODO replace with categoy page*/, { title: 'Express' });
+
+
+});
+
+router.put('/:id/edit', function(req, res, next) {
+
+  res.render('category.ejs'/*TODO replace with categoy page*/, { title: 'Express' });
+
+
+});
+
+router.get('/:id/destroy', function(req, res, next) {
+
+  res.render('category.ejs'/*TODO replace with categoy page*/, { title: 'Express' });
+
+
+});
+
+module.exports = router;
