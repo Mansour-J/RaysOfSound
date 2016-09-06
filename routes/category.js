90bd6a91f8b5f9bc8befa0c5a70617083dd4b3f3
vanvljosh <vanvljosh@ecs.vuw.ac.nz>
2016-09-06 15:01:19 +1200

    Categories page images now show

--- routes/category.js
+++ routes/category.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../lib/db');
-var sequelize = require('sequelize');
 
 router.get('/', function(req, res, next) {
     console.log("")
@@ -9,8 +8,6 @@ router.get('/', function(req, res, next) {
 
 router.get('/:id/view', function(req, res, next) {
 
-    /*  SELECT * FROM Items WHERE item_id='id'
-            INNER JOIN Audio ON Items.item_id=Audio.item_id */
     db.Audio.findAll().then(function (audio){
       db.Item.findAll({
             where: {
@@ -22,18 +19,8 @@ router.get('/:id/view', function(req, res, next) {
             res.render('category.ejs', { title: 'Express', items: items});
         });
     });
+    
 });
-    //
-    // var items = db.Item.findAll({
-    //     where: {
-    //         category_id: req.params.id
-    //     }
-    // }).then(function(items){
-    //
-    //     console.log(items);
-    //
-    //     res.render('category.ejs', { title: 'Express', items: items, audio: audio});
-    // });
 
 router.get('/:id/edit', function(req, res, next) {
 	
