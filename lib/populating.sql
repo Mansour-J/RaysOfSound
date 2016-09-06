8a5772e06bdd4d75ca4e1ca653e04cbc67c27486
Josh Gomez <josh.gomez72@gmail.com>
2016-09-06 13:17:17 +1200

    Fixed duplicating categories

--- lib/populating.sql
+++ lib/populating.sql
@@ -1,34 +1,75 @@
 --Using user _id 1 for all items
 
 --Gagana Samoa
+INSERT INTO item (category_id, item_name, location, description, image, user_id) 
+VALUES (4, 'O Mamanu o le Siapo', '[SAMO.LAN.5627]', 'KLearn about the meaning of designs found in Siapo, explained as an example of Samoan visual language.
+Faaliliuina e Galumalemana Hunkin, Tūsia e Caroline Lolegi Vercoe, O ata na tūsia e Jeff Belt', 'images/patterns/Deirdre_Gardiner.jpg', 1);
+
+INSERT INTO item (category_id, item_name, location, description, image, user_id) 
+VALUES (4, 'Pese Samoa', '[SAMO.MUS.1812]', 'This collection has composition dates spanning 100 years, and represent items known and used throughout Samoa.
+With the kind permission of Glenys Daniell, performed by Kotuku Singers, illustrations Jennifer Cooper', 'images/patterns/Deirdre_Gardiner.jpg', 1);
+
+INSERT INTO item (category_id, item_name, location, description, image, user_id) 
+VALUES (4, 'O Mamanu o le Siapo', '[SAMO.LAN.5627]', 'KLearn about the meaning of designs found in Siapo, explained as an example of Samoan visual language.
+Faaliliuina e Galumalemana Hunkin, Tūsia e Caroline Lolegi Vercoe, O ata na tūsia e Jeff Belt', 'images/patterns/Deirdre_Gardiner.jpg', 1);
+
+INSERT INTO item (category_id, item_name, location, description, image, user_id) 
+VALUES (4, 'O Mamanu o le Siapo', '[SAMO.LAN.5627]', 'KLearn about the meaning of designs found in Siapo, explained as an example of Samoan visual language.
+Faaliliuina e Galumalemana Hunkin, Tūsia e Caroline Lolegi Vercoe, O ata na tūsia e Jeff Belt', 'images/patterns/Deirdre_Gardiner.jpg', 1);
+
+INSERT INTO item (category_id, item_name, location, description, image, user_id) 
+VALUES (4, 'O Mamanu o le Siapo', '[SAMO.LAN.5627]', 'KLearn about the meaning of designs found in Siapo, explained as an example of Samoan visual language.
+Faaliliuina e Galumalemana Hunkin, Tūsia e Caroline Lolegi Vercoe, O ata na tūsia e Jeff Belt', 'images/patterns/Deirdre_Gardiner.jpg', 1);
+
+INSERT INTO item (category_id, item_name, location, description, image, user_id) 
+VALUES (4, 'O Mamanu o le Siapo', '[SAMO.LAN.5627]', 'KLearn about the meaning of designs found in Siapo, explained as an example of Samoan visual language.
+Faaliliuina e Galumalemana Hunkin, Tūsia e Caroline Lolegi Vercoe, O ata na tūsia e Jeff Belt', 'images/patterns/Deirdre_Gardiner.jpg', 1);
+
+INSERT INTO item (category_id, item_name, location, description, image, user_id) 
+VALUES (4, 'O Mamanu o le Siapo', '[SAMO.LAN.5627]', 'KLearn about the meaning of designs found in Siapo, explained as an example of Samoan visual language.
+Faaliliuina e Galumalemana Hunkin, Tūsia e Caroline Lolegi Vercoe, O ata na tūsia e Jeff Belt', 'images/patterns/Deirdre_Gardiner.jpg', 1);
+
+INSERT INTO item (category_id, item_name, location, description, image, user_id) 
+VALUES (4, 'O Mamanu o le Siapo', '[SAMO.LAN.5627]', 'KLearn about the meaning of designs found in Siapo, explained as an example of Samoan visual language.
+Faaliliuina e Galumalemana Hunkin, Tūsia e Caroline Lolegi Vercoe, O ata na tūsia e Jeff Belt', 'images/patterns/Deirdre_Gardiner.jpg', 1);
+
+INSERT INTO item (category_id, item_name, location, description, image, user_id) 
+VALUES (4, 'O Mamanu o le Siapo', '[SAMO.LAN.5627]', 'KLearn about the meaning of designs found in Siapo, explained as an example of Samoan visual language.
+Faaliliuina e Galumalemana Hunkin, Tūsia e Caroline Lolegi Vercoe, O ata na tūsia e Jeff Belt', 'images/patterns/Deirdre_Gardiner.jpg', 1);
 
 
 
 --Maori - Tamariki
-INSERT INTO item (category_id, item_name, location, description, image, user_id) VALUES (1, 'Ngā Manu i Runga i te Rākau', '[MAOR.3699.B/8]', 'Ko ngā kōrero nā Bill Gillies 
+INSERT INTO item (category_id, item_name, location, description, image, user_id) 
+VALUES (1, 'Ngā Manu i Runga i te Rākau', '[MAOR.3699.B/8]', 'Ko ngā kōrero nā Bill Gillies 
 Ko ngā whakaāhua nā Murray Grimsdale', 'images/patterns/Deirdre_Gardiner.jpg', 1);
 
-INSERT INTO item (category_id, item_name, location, description, image, user_id) VALUES (1, 'He Āporo Mā Hōiho', '[MAOR.3699.B/1]', 'An unlikely helper saves the day of a hungry horse! Read more to find out!<br>
+INSERT INTO item (category_id, item_name, location, description, image, user_id) 
+VALUES (1, 'He Āporo Mā Hōiho', '[MAOR.3699.B/1]', 'An unlikely helper saves the day of a hungry horse! Read more to find out!<br>
 Ko ngā kōrero nā Henare Everitt<br>
 Ko ngā pikitia nā Deirdre Gardiner', 'images/patterns/Deirdre_Gardiner.jpg', 1);
 
-INSERT INTO item (category_id, item_name, location, description, image, user_id) VALUES (1, 'Te Whakawhiti i te Rori', '[MAOR.3699.B/3]', 'The story of a chook and the hardship of crossing roads in heavy traffic<br>
+INSERT INTO item (category_id, item_name, location, description, image, user_id) 
+VALUES (1, 'Te Whakawhiti i te Rori', '[MAOR.3699.B/3]', 'The story of a chook and the hardship of crossing roads in heavy traffic<br>
 Ko ngā kōrero nā Mākere Rogers<br>
 nā Dick Frizzell ngā pikitia', 'images/patterns/Deirdre_Gardiner.jpg', 1);
 
-INSERT INTO item (category_id, item_name, location, description, image, user_id) VALUES (1, 'He Kai mā te Ika', '[MAOR.3699.B/4]', 'Follow the adventures of a family of worms from their home to the sea.<br>
+INSERT INTO item (category_id, item_name, location, description, image, user_id) 
+VALUES (1, 'He Kai mā te Ika', '[MAOR.3699.B/4]', 'Follow the adventures of a family of worms from their home to the sea.<br>
 Ko ngā kōrero nā Henare Everitt<br>
 Ko ngā pikitia nā Penny Martin', 'images/patterns/Deirdre_Gardiner.jpg', 1);
 
-INSERT INTO item (category_id, item_name, location, description, image, user_id) VALUES (1, 'Pāpā, Māmā', '[MAOR.LAN.3699.B/5]', 'Mum and dad work hard at making us happy, find out how.<br>
+INSERT INTO item (category_id, item_name, location, description, image, user_id) 
+VALUES (1, 'Pāpā, Māmā', '[MAOR.LAN.3699.B/5]', 'Mum and dad work hard at making us happy, find out how.<br>
 Ko ngā kōrero nā Piatarihi Yates me Henare Everitt<br>
 Ko ngā pikitia nā Deirdre Gardiner me Roger Hart', 'images/patterns/Deirdre_Gardiner.jpg', 1);
 
-INSERT INTO item (category_id, item_name, location, description, image, user_id) VALUES (1, 'Pererika Te Poraka', '[[MAOR.3699.B/7]', 'A lost baby frog is looking for its mother in the reeds and finds help along the way.<br>
+INSERT INTO item (category_id, item_name, location, description, image, user_id) 
+VALUES (1, 'Pererika Te Poraka', '[[MAOR.3699.B/7]', 'A lost baby frog is looking for its mother in the reeds and finds help along the way.<br>
 Ko ngā kōrero nā Piatarihi Yates<br>
 Ko ngā pikitia nā Sally McAra', 'images/patterns/Deirdre_Gardiner.jpg', 1);
 
-INSERT INTO item (category_id, item_name, location, description, image, user_id) VALUES (1, 'Ko Māia me te Mangumangu Taipō', '[MAOR.3731.B/2]', 'An ugly, toothless forest goblin is trying to scare a little girl… but he’s not that scary after all. Part of Te Tautoko 15.<br>
+INSERT INTO item (category_id, item_name, location, description, image, user_id) 
+VALUES (1, 'Ko Māia me te Mangumangu Taipō', '[MAOR.3731.B/2]', 'An ugly, toothless forest goblin is trying to scare a little girl… but he’s not that scary after all. Part of Te Tautoko 15.<br>
 Ko ngā kōrero nā Erana Lee Brewerton<br>
 Ko ngā pikitia nā Wendy Hodder', 'images/patterns/Deirdre_Gardiner.jpg', 1);
-
