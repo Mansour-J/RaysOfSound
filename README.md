ff24bd75062a675fb9eba3ead58dddfc3bdcd805
vanvljosh <vanvljosh@ecs.vuw.ac.nz>
2016-09-06 14:27:51 +1200

    Work on categories page, dosn't get data passed from js file correctly.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Rays of Sound
 
 This website was born out of the determination of Victoria University of Wellington's Language Learning Centre to save some older, but valuable stories and songs on tape and make them more accessible for everyone to enjoy. After securing permission from the copyright holders for digitisation, we created this website and embedded the audio material for online streaming. The result is a digital audio-book shelf which breathes new life into precious, but long-underused resources.
 
-This project is under developmen for our SWEN302 agile methods course.
+This project is under development for our SWEN302 agile methods course.
--- routes/category.js
+++ routes/category.js
@@ -11,28 +11,29 @@ router.get('/:id/view', function(req, res, next) {
 
     /*  SELECT * FROM Items WHERE item_id='id'
             INNER JOIN Audio ON Items.item_id=Audio.item_id */
-    var audio = db.Audio.findAll();
-
-    var items = db.Item.findAll({
-        where: {
-            category_id: req.params.id
-        }
-    }).then(function(items){
-        res.render('category.ejs', { title: 'Express', items: items, audio: audio});
+    db.Audio.findAll().then(function (audio){
+      db.Item.findAll({
+            where: {
+                category_id: req.params.id
+            }
+        }).then(function (items){
+            console.log(audio);
+            console.log(items);
+            res.render('category.ejs', { title: 'Express', items: items});
+        });
     });
-
-
-
-    /*
-
-
-    //console.log("ITEMS: "+items[0]);
-    //console.log("AUDIO: "+audio[0]);
-
-    res.render('category.ejs', { title: 'Express', data: [items, audio] })
-
-*/
 });
+    //
+    // var items = db.Item.findAll({
+    //     where: {
+    //         category_id: req.params.id
+    //     }
+    // }).then(function(items){
+    //
+    //     console.log(items);
+    //
+    //     res.render('category.ejs', { title: 'Express', items: items, audio: audio});
+    // });
 
 router.get('/:id/edit', function(req, res, next) {
 	
