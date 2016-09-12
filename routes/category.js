242b1be202f81f444252fe461e83b7e63d0b4632
Josh Gomez <josh.gomez72@gmail.com>
2016-09-13 10:26:41 +1200

    Bug fix, Removed html-to-text library

--- routes/category.js
+++ routes/category.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../lib/db');
-var htmlToText = require('html-to-text');
 
 router.get('/', function(req, res, next) {
     console.log("")
