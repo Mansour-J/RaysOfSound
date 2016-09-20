b2fbd1d4049f3976e0b73d8eec72268b24aa4c26
Zachariah Macfarlane <zachmacfarlane@live.com>
2016-09-20 12:02:54 +1200

    commented out unused tests

--- tests/routes/categoryTests.js
+++ tests/routes/categoryTests.js
@@ -1,13 +1,13 @@
-var chai = require("chai");
-var assert = chai.assert;
-var expect = chai.expect;
-var should = chai.should();
+// var chai = require("chai");
+// var assert = chai.assert;
+// var expect = chai.expect;
+// var should = chai.should();
 
-var request = require('supertest');
-var app = require('express');
-var router = app.Router()
+// var request = require('supertest');
+// var app = require('express');
+// var router = app.Router()
 
-var Category = require("./../../routes/category.js");
+// var Category = require("./../../routes/category.js");
 
 
 //TODO test for specific category id pages
--- tests/routes/manageTests.js
+++ tests/routes/manageTests.js
@@ -1,23 +1,23 @@
-var chai = require("chai");
-var assert = chai.assert;
-var expect = chai.expect;
-var should = chai.should();
+// var chai = require("chai");
+// var assert = chai.assert;
+// var expect = chai.expect;
+// var should = chai.should();
 
-var request = require('supertest');
-var app = require('express');
-var router = app.Router()
+// var request = require('supertest');
+// var app = require('express');
+// var router = app.Router()
 
-var Manage = require("./../../routes/manage.js");
+// var Manage = require("./../../routes/manage.js");
 
-describe('GET /manage', function() {
-  it('load manage page', function() {
+// describe('GET /manage', function() {
+//   it('load manage page', function() {
 
-    request(app)
-        .get('/manage')
-        .expect(200, "ok")
-        .end(function(err, res){
-           if (err) throw err;
-        });
+//     request(app)
+//         .get('/manage')
+//         .expect(200, "ok")
+//         .end(function(err, res){
+//            if (err) throw err;
+//         });
 
-  });
-});
+//   });
+// });
