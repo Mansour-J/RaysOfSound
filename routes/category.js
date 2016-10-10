de8529c443c292d67969189f733f2f59108b167a
Mansour Javaher <javaher.mansour@gmail.com>
2016-10-11 11:31:17 +1300

    DONE: Pagination is done -- both functionality and fron-end

--- routes/category.js
+++ routes/category.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:id/view', function(req, res, next) {
-
+/*
     db.Audio.findAll().then(function (audio){
       db.Item.findAll({
             where: {
@@ -26,19 +26,31 @@ router.get('/:id/view', function(req, res, next) {
               }
             });
         });
-    });
+    });*/
+
+    res.redirect('/category/' + req.params.id +  '/view/1');
 
 });
 
 router.get('/:id/view/:page', function(req, res, next) {
     var page = req.params.page;
+    var itemPerPage = 6;
+    var start = (page - 1) * 6;
 
-    var start = (page - 1) * 3;
-    var itemPerPage = 3;
-    console.log
-    console.log("=============================================== Page " + page + " ========================================");
-
+    var itemLength = 0;
     if(!isNaN(page)){
+        db.Item.findAll({
+                where: {
+                    category_id: req.params.id
+                }
+            }).then(function (itemAll) {
+            console.log("=============================================== ITEMLENGTH " + itemLength + " ========================================");
+
+            itemLength = itemAll.length;
+            console.log("=============================================== ITEMLENGTH " + itemLength + " ========================================");
+
+
+
         db.Audio.findAll().then(function (audio){
             db.Item.findAll({
                 where: {
@@ -49,13 +61,15 @@ router.get('/:id/view/:page', function(req, res, next) {
             }).then(function (items){
                 db.Category.findAll().then(function(categories){
                     if (items.length != 0){
-                        res.render('category.ejs', { title: 'Express', items: items, audio: audio, data: categories, user:req.user});
+                        res.render('category.ejs', { title: 'Express', itemLength: itemLength, items: items, audio: audio, data: categories, user:req.user});
                     }
                     else {
                         res.redirect('/error');
                     }
                 });
             });
+        });
+
         });
     }
 
