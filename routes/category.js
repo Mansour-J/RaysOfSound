cee8181368340f009a574e19013c95b4f7a1d6bb
Mansour Javaher <javaher.mansour@gmail.com>
2016-10-11 10:27:24 +1300

    IN-PROGRESS: Pagination is almost done -- back-end fucntionality is done needs to implement in front-end

--- routes/category.js
+++ routes/category.js
@@ -30,6 +30,37 @@ router.get('/:id/view', function(req, res, next) {
 
 });
 
+router.get('/:id/view/:page', function(req, res, next) {
+    var page = req.params.page;
+
+    var start = (page - 1) * 3;
+    var itemPerPage = 3;
+    console.log
+    console.log("=============================================== Page " + page + " ========================================");
+
+    if(!isNaN(page)){
+        db.Audio.findAll().then(function (audio){
+            db.Item.findAll({
+                where: {
+                    category_id: req.params.id
+                },
+                limit: itemPerPage,
+                offset: start
+            }).then(function (items){
+                db.Category.findAll().then(function(categories){
+                    if (items.length != 0){
+                        res.render('category.ejs', { title: 'Express', items: items, audio: audio, data: categories, user:req.user});
+                    }
+                    else {
+                        res.redirect('/error');
+                    }
+                });
+            });
+        });
+    }
+
+});
+
 router.get('/:id/edit', function(req, res, next) {
 
   res.render('category.ejs'/*TODO replace with categoy page*/, { title: 'Express' });
