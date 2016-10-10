14506b94766b7eaba8df66248fdb95ad0931e69f
vanvljosh <vanvljosh@ecs.vuw.ac.nz>
2016-10-11 12:57:54 +1300

    Re-added editing of items to item list

--- routes/admin.js
+++ routes/admin.js
@@ -9,13 +9,19 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.get('/allitems', function(req, res, next) {
+router.get('/view/allitems', function(req, res, next) {
     db.Category.findAll().then(function (categories){
         db.Item.findAll().then(function (items){
             db.Audio.findAll().then(function (audio){
                 res.render('allitems.ejs', { title: 'Express', data: categories, items: items, audio: audio });
             });
         });
+    });
+});
+
+router.get('/manage/categories', function(req, res, next) {
+    db.Category.findAll().then(function(categories){
+        res.render('manageCats.ejs', { title: 'Express', data: categories});
     });
 });
 
