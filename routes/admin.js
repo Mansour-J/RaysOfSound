b0849acc4a956ddfba98c2c6265c65411581c782
vanvljosh <vanvljosh@ecs.vuw.ac.nz>
2016-10-11 16:52:34 +1300

    Can edit category names.

--- routes/admin.js
+++ routes/admin.js
@@ -1,28 +1,55 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../lib/db');
+var helper = require('../lib/helper');
 
-router.get('/', function(req, res, next) {
-    db.Category.findAll().then(function(categories){
-        res.render('admin.ejs', { title: 'Express', data: categories});
+
+router.get('/', function (req, res, next) {
+    db.Category.findAll().then(function (categories) {
+        res.render('admin.ejs', {title: 'Express', data: categories});
     });
 });
 
 
-router.get('/view/allitems', function(req, res, next) {
-    db.Category.findAll().then(function (categories){
-        db.Item.findAll().then(function (items){
-            db.Audio.findAll().then(function (audio){
-                res.render('allitems.ejs', { title: 'Express', data: categories, items: items, audio: audio });
+router.get('/view/allitems', function (req, res, next) {
+    db.Category.findAll().then(function (categories) {
+        db.Item.findAll().then(function (items) {
+            db.Audio.findAll().then(function (audio) {
+                res.render('allitems.ejs', {title: 'Express', data: categories, items: items, audio: audio});
             });
         });
     });
 });
 
-router.get('/manage/categories', function(req, res, next) {
-    db.Category.findAll().then(function(categories){
-        res.render('manageCats.ejs', { title: 'Express', data: categories});
+router.get('/manage/categories', function (req, res, next) {
+    db.Category.findAll().then(function (categories) {
+        res.render('manageCats.ejs', {title: 'Express', data: categories});
     });
+});
+
+//router.post('/manage/categories', helper.isAuthenicated, function (req, res, next) {
+router.post('/manage/categories', function (req, res, next) {
+        var loggedIn;
+
+        var newId = [];
+        var newTitle = [];
+
+        db.Category.findAll().then(function (category) {
+            for (var i = 0; i < category.length; i++) {
+                newId[i] = category[i].id;
+                newTitle[i] = category[i].title;
+            }
+            for (var i = 0; i < category.length; i++) {
+                 db.Category.update({
+                    title: req.body['input'+newId[i]]
+                 },
+                 {
+                    where: { id: newId[i] }
+                 });
+            }
+        }).then(function () {
+            res.redirect('./categories');
+        });
 });
 
 
